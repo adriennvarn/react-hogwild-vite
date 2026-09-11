c833ab1522969d4cfdf4f127c3d60776bc4be053
Adrienn Varn <adriennvarn@gmail.com>
2026-09-11 18:22:51 -0400

    filter greased only

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 import Nav from "./Nav"
 import HogCard from "./HogCard"
+import FilterBar from "./FilterBar"
 import { v4 as uuid } from "uuid"
 
 import hogs from "../porkers_data"
 
 function App() {
+    const [greasedOnly, setGreasedOnly] = useState(false)
+    
+    const filterGreased = hogs.filter(hog => hog.greased)
+
     return (
         <div className="App">
             <Nav />
+            <FilterBar greasedOnly={greasedOnly} setGreasedOnly={setGreasedOnly} />
             <div className="ui grid container">
-                {hogs.map(hog => (
+                {(greasedOnly ? filterGreased : hogs).map(hog => (
                     <HogCard hog={hog} key={uuid()} />
                 ))}
             </div>
