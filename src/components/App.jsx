d90e127a9f07505a6a57a227134eb5e2d65a4596
Adrienn Varn <adriennvarn@gmail.com>
2026-09-11 18:52:17 -0400

    filter implemented; correctly keeps hogs hidden on filter change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,22 +2,45 @@ import React, { useState } from "react"
 import Nav from "./Nav"
 import HogCard from "./HogCard"
 import GreasedBar from "./GreasedBar"
+import SortBar from "./SortBar"
 import { v4 as uuid } from "uuid"
 
 import hogs from "../porkers_data"
 
 function App() {
+    const [hiddenHogs, setHiddenHogs] = useState([])
     const [greasedOnly, setGreasedOnly] = useState(false)
-    
-    const filterGreased = hogs.filter(hog => hog.greased)
+    const [sort, setSort] = useState("all")
+
+    function sortedHogs() {
+        const filterGreasedHogs = (greasedOnly ? hogs.filter(hog => hog.greased) : hogs)
+        const filterHiddenHogs = filterGreasedHogs.filter(hog => !hiddenHogs.includes(hog))
+        if (sort === "all") {
+            return filterHiddenHogs
+        }
+        else if (sort === "name") {
+            return filterHiddenHogs.toSorted((a, b) => a.name.localeCompare(b.name))
+        }
+        else if (sort === "weight") {
+            return filterHiddenHogs.toSorted((a, b) => a.weight - b.weight)
+        }
+        else {
+            console.log("Error in sortedHogs()")
+        }
+    }
+
+    function hideHog(hog) {
+        setHiddenHogs([...hiddenHogs, hog])
+    }
 
     return (
         <div className="App">
             <Nav />
             <GreasedBar greasedOnly={greasedOnly} setGreasedOnly={setGreasedOnly} />
+            <SortBar sort={sort} setSort={setSort} />
             <div className="ui grid container">
-                {(greasedOnly ? filterGreased : hogs).map(hog => (
-                    <HogCard hog={hog} key={uuid()} />
+                {sortedHogs().map(hog => (
+                    <HogCard hog={hog} key={uuid()} hideHog={(hog) => hideHog(hog)} />
                 ))}
             </div>
         </div>
