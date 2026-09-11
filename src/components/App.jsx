1af78dbc09d3d3705ced771ba96ca14b5bd751b5
Adrienn Varn <adriennvarn@gmail.com>
2026-09-11 18:24:18 -0400

    renaming

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import Nav from "./Nav"
 import HogCard from "./HogCard"
-import FilterBar from "./FilterBar"
+import GreasedBar from "./GreasedBar"
 import { v4 as uuid } from "uuid"
 
 import hogs from "../porkers_data"
@@ -14,7 +14,7 @@ function App() {
     return (
         <div className="App">
             <Nav />
-            <FilterBar greasedOnly={greasedOnly} setGreasedOnly={setGreasedOnly} />
+            <GreasedBar greasedOnly={greasedOnly} setGreasedOnly={setGreasedOnly} />
             <div className="ui grid container">
                 {(greasedOnly ? filterGreased : hogs).map(hog => (
                     <HogCard hog={hog} key={uuid()} />
